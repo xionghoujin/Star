67678ff73a56ce310a84caf38477e68c603947f1
xionghoujin <892989054@qq.com>
2018-01-29 10:50:52 +0800

    Initial commit

---
+++ src/main/java/org/start/chapter1/model/Customer.java
@@ -0,0 +1,7 @@
+package org.start.chapter1.model;
+
+/**
+ * Created by XHJ on 2018/1/28.
+ */
+public class Customer {
+}
---
+++ src/main/java/org/start/chapter1/service/CustomerCreateService.java
@@ -0,0 +1,7 @@
+package org.start.chapter1.service;
+
+/**
+ * Created by XHJ on 2018/1/28.
+ */
+public class CustomerCreateService {
+}
